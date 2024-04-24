1a6b28a4a4d0ecf26b844a1a2eabd3e15e24d441
andrxpie <andrxpie@gmail.com>
2024-04-24 18:22:13 +0300

    Added Back button on About page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
+import { SwapLeftOutlined } from "@ant-design/icons";
 
 export default function About() {
+  const navigate = useNavigate();
+
   return (
-    <div>About Privat24</div>
-  )
+    <>
+      <h2 className="return-zone">
+        <Button type="dashed" onClick={() => navigate(-1)}>
+          <SwapLeftOutlined />
+        </Button>
+        <span>Back</span>
+      </h2>
+      <div>About Privat24</div>
+    </>
+  );
 }
