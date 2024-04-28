bd0ebfecc0887c73f2d04fef5313535dd62d4e3c
Андрій Грицюк <venganpro12@gmail.com>
2024-04-28 22:58:19 +0300

    Included ConfigProvider

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import Layout from './components/Layout';
-import Choise from './components/Choise';
-import NewCardForm from './components/NewCardForm';
-import NewTransactionForm from './components/NewTransactionForm';
-import PayNumForm from './components/PayNumForm';
-import Archive from './components/Archive';
-import Home from './components/Home';
-import Cards from './components/Cards';
-import About from './components/About';
-import NoPage from './components/NoPage';
+import React from "react";
+import { ConfigProvider } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Choise from "./components/Choise";
+import NewCardForm from "./components/NewCardForm";
+import NewTransactionForm from "./components/NewTransactionForm";
+import PayNumForm from "./components/PayNumForm";
+import Archive from "./components/Archive";
+import Home from "./components/Home";
+import Cards from "./components/Cards";
+import About from "./components/About";
+import NoPage from "./components/NoPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="cards" element={<Cards />} />
-          <Route path="new" element={<Choise />} />
-          <Route path="new/card" element={<NewCardForm />} />
-          <Route path="new/transaction" element={<NewTransactionForm />} />
-          <Route path="new/paynum" element={<PayNumForm />} />
-          <Route path="archive" element={<Archive />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#ff4f6c',
+            colorFillSecondary: 'white'
+          },
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="cards" element={<Cards />} />
+              <Route path="new" element={<Choise />} />
+              <Route path="new/card" element={<NewCardForm />} />
+              <Route path="new/transaction" element={<NewTransactionForm />} />
+              <Route path="new/paynum" element={<PayNumForm />} />
+              <Route path="archive" element={<Archive />} />
+              <Route path="about" element={<About />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Layout as AntdLayout, Menu } from 'antd';
 import { Link, useLocation  } from 'react-router-dom';
-import { CreditCardOutlined, HomeOutlined, InfoCircleOutlined, PlusCircleOutlined, ProductOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import { CreditCardOutlined, HomeOutlined, InfoCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import '../App.scss'
 
 const { Header: AntdHeader } = AntdLayout;
 
